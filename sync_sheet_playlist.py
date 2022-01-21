0472e62e70e2d6e42f4bef08e5b729b391142a62
Tatu Lahtela <tatu.lahtela@vincit.fi>
2022-01-21 11:32:18 +0200

    2022

--- sync_sheet_playlist.py
+++ sync_sheet_playlist.py
@@ -4,15 +4,14 @@ import os
 import requests
 import spotipy
 import json
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import spotipy.util as util
 
 import re
 
-GOOGLE_SHEET="https://spreadsheets.google.com/feeds/cells/1fQFg52uaojpRCz29EzSHVpsX5SYVJ2VN8IuKs9XA5W8/1/public/full?alt=json"
-GOOGLE_SHEET_PROGROG="https://spreadsheets.google.com/feeds/cells/1fQFg52uaojpRCz29EzSHVpsX5SYVJ2VN8IuKs9XA5W8/2/public/full?alt=json"
-PLAYLIST="1JhoE1TBSTYsP3i2l5F5Yd" # https://open.spotify.com/playlist/1JhoE1TBSTYsP3i2l5F5Yd?si=BF_eFvalTx6wfFByf7PfIA
-USER_ID="ttration"
+GOOGLE_SHEET = f"https://sheets.googleapis.com/v4/spreadsheets/1fQFg52uaojpRCz29EzSHVpsX5SYVJ2VN8IuKs9XA5W8?ranges=2022+Prog-metal&fields=sheets(data(rowData(values(hyperlink))))&key={os.environ['GOOGLE_API_KEY']}"
+PLAYLIST = "39gJ3OUrocQ3IdTehoAbMl" # 2022
+USER_ID = "ttration"
 
 def get_spotify_playlist_tracks(sp, playlist, offset=0):
     playlist_data = sp.playlist_tracks(PLAYLIST, limit=100, offset=offset)
@@ -24,16 +23,14 @@ def get_spotify_playlist_tracks(sp, playlist, offset=0):
                 spotify_list_albums.append(track['album']['id'])
         spotify_list_albums = spotify_list_albums + get_spotify_playlist_tracks(sp, playlist, offset+100)
     return spotify_list_albums
-    
+
+
 def get_album_list_from_sheet(sheet):
-    google_sheet_data = json.loads(requests.get(sheet).text)
-    albums = []
-    album_re = re.compile("spotify\.com\/album\/(.*?)[\"|\?]")
-    for entry in google_sheet_data['feed']['entry']:
-        cell = entry['gs$cell']
-        spotify_match = album_re.search(cell['inputValue'])
-        if cell['col'] == "9" and spotify_match:
-            albums.append(spotify_match[1])
+    req = requests.get(sheet)
+    if req.status_code > 300:
+        return []
+    data = req.text
+    albums = re.findall(r"(https.*spotify.*album.*)\"", data)
     return albums
 
 
@@ -42,24 +39,27 @@ def add_albums_to_spotify_playlist(sp, username, playlist_id, albums):
         ids = []
         sp_album = sp.album_tracks(album)
         if 'items' in sp_album:
-            for track in sp_album['items']:            
+            for track in sp_album['items']:
                 ids.append(track['id'])
         elif 'tracks' in sp_album:
             for track in sp_album['tracks']['items']:
                 ids.append(track['id'])
         sp.user_playlist_add_tracks(username, playlist_id, ids)
 
+
 scope = 'playlist-modify-public'
 token = util.prompt_for_user_token(USER_ID, scope)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    spotify_list_albums = get_spotify_playlist_tracks(sp=sp, playlist=PLAYLIST, offset=0)
-    google_sheet_albums = get_album_list_from_sheet(GOOGLE_SHEET) + get_album_list_from_sheet(GOOGLE_SHEET_PROGROG)
-    #print(google_sheet_albums)
-    missing = set(google_sheet_albums) - set(spotify_list_albums)
-    if len(missing) > 0:
-        print("Added %s" % str(missing))
-        add_albums_to_spotify_playlist(sp=sp, username=USER_ID, playlist_id=PLAYLIST,albums=missing)
-
-
+if __name__ == '__main__':
+    if token:
+        sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ["SPOTIPY_CLIENT_ID"],
+                                                       client_secret=os.environ["SPOTIPY_CLIENT_SECRET"],
+                                                       redirect_uri=os.environ["SPOTIPY_REDIRECT_URI"],
+                                                       scope="user-library-read playlist-modify-private"))
+        spotify_list_albums = get_spotify_playlist_tracks(sp=sp, playlist=PLAYLIST, offset=0)
+        google_sheet_albums = get_album_list_from_sheet(GOOGLE_SHEET)  # + get_album_list_from_sheet(GOOGLE_SHEET_PROGROG)
+        # print(google_sheet_albums)
+        missing = set(map(lambda x: re.findall(r"album/(.*?)(\?|$)", x)[0][0], google_sheet_albums)) - set(spotify_list_albums)
+        if len(missing) > 0:
+            print("Added %s" % str(missing))
+            add_albums_to_spotify_playlist(sp=sp, username=USER_ID, playlist_id=PLAYLIST, albums=missing)
